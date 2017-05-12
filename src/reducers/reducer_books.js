e88b833fad24871b23a661159e4e25c9cfe89883
Ivan Fertikov <i-fertikov@marya.ru>
2017-05-12 12:29:04 +0400

    provide react-redux connection, create index.js

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{title: 'Javascript: The Good Parts'},
+		{title: 'Harry Potter and the Chamber of Secrets'},
+		{title: 'The Dark Tower'},
+		{title: 'Eloquent Ruby'}
+	];
+}
